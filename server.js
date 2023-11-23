043de900f590f90abf39471f13b2b2b2212df81a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-11-23 03:31:09 +0100

    made some changes

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const http = require ('http');
 const app = require('./app')
 
-const productRoutes = require('./api/routes/products')
-
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
