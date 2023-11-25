ca71c1d6a0b1e1400012ae4b998719cddb64706a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-11-26 00:28:58 +0100

    added  a json message

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -15,7 +15,7 @@ router.post('/', (req, res, next) => {
 
 router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
-        message: 'Orders were fetched'
+        message: 'Order details'
     })
 });
 
