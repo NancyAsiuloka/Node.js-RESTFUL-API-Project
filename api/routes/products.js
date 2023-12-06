0b26cc7ea88bf1420d6827b3aa8641b41aa4c4f2
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-06 06:14:07 +0100

    added a comment

--- api/routes/products.js
+++ api/routes/products.js
@@ -38,7 +38,11 @@ router.get("/:productId", (req, res, next) => {
   .then(doc => {
     console.log(doc);
     if (doc) {
-
+      res.status(200).json(doc)
+    } else {
+      res.status(404).json({
+        message: 'No valid entry found for provided ID'
+      })
     }
     res.status(200).json(doc);
   })
