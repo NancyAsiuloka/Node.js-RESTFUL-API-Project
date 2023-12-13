045dc185e43ced844e64794157df43307e1d9f5b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-13 03:09:41 +0100

    made some changes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -73,6 +73,11 @@ router.post("/", (req, res, next) => {
 
 router.get("/:orderId", (req, res, next) => {
   Order.findById(req.params.orderId).exec().then(order => {
+    if(!order){
+      return res.status(404).json({
+        message: 'Order not found'
+      })
+    }
     res.status(200).json({
       order: order,
       request: {
@@ -82,15 +87,31 @@ router.get("/:orderId", (req, res, next) => {
     })
   }).catch(err => {
     res.status(500).json({
-      message: 'No valid entry found for the given ID.'
+      error: err
     })
   });
 });
 
 router.delete("/:orderId", (req, res, next) => {
-  res.status(200).json({
-    message: "Order deleted",
-    orderId: req.params.orderId,
+  Order.remove({ _id: req.params.orderId})
+  .exec()
+  .then(result => {
+    res.status(200).json({
+      message: 'Order deleted',
+      request: {
+        type: 'POST',
+        url: 'http://localhost:3000/orders',
+        body: {
+          productId: 'ID',
+          quantity: 'Number'
+        }
+      }
+    })
+  })
+  .catch(err => {
+    res.status(500).json({
+      error: err
+    })
   });
 });
 
