6feb7eec2152f8d85fa35f9957d1f885e7284a7d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-15 03:56:26 +0100

    added a comment

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     cb(null, new Date().toISOString + file.originalname);
-  }
+}
 })
 
 const upload = multer({storage: storage})
