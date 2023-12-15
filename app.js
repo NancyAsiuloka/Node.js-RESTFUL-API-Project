e56067e27a1b6b64604d3b1c2ea0909b50ac722d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-15 04:47:46 +0100

    added a comment

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect(
 // mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));
-app.use(express.static('uploads'))
+app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
