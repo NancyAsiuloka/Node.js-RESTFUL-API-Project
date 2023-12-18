894c99ab32baec485a2b21176a13f346affb8436
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-18 04:34:45 +0100

    added a comment

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,32 +9,10 @@ const OrdersController = require('../controllers/order')
 // Handle incoming GET request to /orders
 router.get("/", checkAuth, OrdersController.orders_get_all );
 
-
 router.post("/", checkAuth, OrdersController.orders_create_order);
 
-router.get("/:orderId", checkAuth, );
+router.get("/:orderId", checkAuth, OrdersController.orders_get_order);
 
-router.delete("/:orderId", checkAuth, (req, res, next) => {
-  Order.remove({ _id: req.params.orderId})
-  .exec()
-  .then(result => {
-    res.status(200).json({
-      message: 'Order deleted',
-      request: {
-        type: 'POST',
-        url: 'http://localhost:3000/orders',
-        body: {
-          productId: 'ID',
-          quantity: 'Number'
-        }
-      }
-    })
-  })
-  .catch(err => {
-    res.status(500).json({
-      error: err
-    })
-  });
-});
+router.delete("/:orderId", checkAuth, );
 
 module.exports = router;
