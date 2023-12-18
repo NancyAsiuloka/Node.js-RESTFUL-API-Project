b3eea3898b291dadf1155ff8fc8eb2469e818d11
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-18 04:46:52 +0100

    made some changes

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require('multer')
 const checkAuth = require('../middleware/check-auth')
+const ProductsController = require('../controllers/products.js')
+
 
 // const storage = multer.diskStorage({
 //   destination: (req, file, cb) => {
@@ -32,77 +34,11 @@ const checkAuth = require('../middleware/check-auth')
 
 const upload = multer({dest: 'uploads/'})
 
-
 const Product = require("../models/product.js");
 
-router.get("/", (req, res, next) => {
-  Product.find()
-    .select("name price _id productImage")
-    .exec()
-    .then((docs) => {
-      const response = {
-        count: docs.length,
-        products: docs.map((doc) => {
-          return {
-            name: doc.name,
-            price: doc.price,
-            productImage: doc.productImage,
-            _id: doc._id,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/products/" + doc._id,
-            },
-          };
-        }),
-      };
-      // if (docs.length >= 0) {
-      res.status(200).json(response);
-      // } else {
-      //   res.status(404).json({
-      //     message: "no entries found"
-      //   })
-      // }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.get("/", ProductsController.products_get_all);
 
-router.post("/", checkAuth, upload.single('productImage'), (req, res, next) => {
-  console.log(req.file);
-  const products = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price,
-    productImage: req.file.path
-  });
-  products
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        message: "Created product successfully",
-        createdProduct: {
-          name: result.name,
-          price: result.price,
-          _id: result._id,
-          request: {
-            type: "GET",
-            url: "http://localhost:3000/products/" + result._id,
-          },
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.post("/", checkAuth, upload.single('productImage'), );
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
