c247ee8ae9ca30bd91b9313368c3db2154e824c9
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-18 04:26:15 +0100

    added some changes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,74 +4,13 @@ const mongoose = require("mongoose");
 const Order = require("../models/order");
 const Product = require("../models/product");
 const checkAuth = require('../middleware/check-auth')
+const OrdersController = require('../controllers/order')
 
 // Handle incoming GET request to /orders
-router.get("/", checkAuth, (req, res, next) => {
-  Order.find()
-    .select("product quantity _id ")
-    .populate('product', 'name')
-    .exec()
-    .then((docs) => {
-      res.status(200).json({
-        count: docs.length,
-        orders: docs.map((doc) => {
-          return {
-            _id: doc._id,
-            product: doc.product,
-            quantity: doc.quantity,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/orders/" + docs._id,
-            },
-          };
-        }),
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.get("/", checkAuth, OrdersController.orders_get_all );
 
 
-router.post("/", checkAuth, (req, res, next) => {
-  Product.findById(req.body.productId).then((product) => {
-    if (!product) {
-      return res.status(404).json({
-        message: "Product not found",
-      });
-    }
-    const order = new Order({
-      _id: mongoose.Types.ObjectId(),
-      quantity: req.body.quantity,
-      product: req.body.productId,
-    });
-    return order
-      .save()
-      .then((result) => {
-        console.log(result);
-        res.status(201).json({
-          message: "Order stored",
-          createdOrder: {
-            _id: result._id,
-            product: result.product,
-            quantity: result.quantity,
-          },
-          request: {
-            type: "GET",
-            url: "http://localhost:3000/orders/" + result._id,
-          },
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          error: err,
-        });
-      });
-  });
-});
+router.post("/", checkAuth, );
 
 router.get("/:orderId", checkAuth, (req, res, next) => {
   Order.findById(req.params.orderId)
