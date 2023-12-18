7287f0677971d49cc72214bc85ad74c4e38bb049
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-18 04:37:46 +0100

    made some changes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const Order = require("../models/order");
-const Product = require("../models/product");
 const checkAuth = require('../middleware/check-auth')
 const OrdersController = require('../controllers/order')
 
@@ -13,6 +10,6 @@ router.post("/", checkAuth, OrdersController.orders_create_order);
 
 router.get("/:orderId", checkAuth, OrdersController.orders_get_order);
 
-router.delete("/:orderId", checkAuth, );
+router.delete("/:orderId", checkAuth, OrdersController.orders_delete_order);
 
 module.exports = router;
