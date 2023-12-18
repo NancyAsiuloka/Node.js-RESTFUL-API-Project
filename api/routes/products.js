36496c6bb962b4b7f7d358f8a9dca58207c8fa83
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-18 04:53:05 +0100

    made some changes

--- api/routes/products.js
+++ api/routes/products.js
@@ -38,59 +38,11 @@ const Product = require("../models/product.js");
 
 router.get("/", ProductsController.products_get_all);
 
-router.post("/", checkAuth, upload.single('productImage'), );
+router.post("/", checkAuth, upload.single('productImage'), ProductsController.products_create_product);
 
-router.get("/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  Product.findById(id)
-    .select("name price _id productImage")
-    .exec()
-    .then((doc) => {
-      console.log(doc);
-      if (doc) {
-        res.status(200).json({
-          product: doc,
-          request: {
-            type: "GET",
-            description: "Get all products",
-            url: "http://localhost:3000/products",
-          },
-        });
-      } else {
-        res.status(404).json({
-          message: "No valid entry found for provided ID",
-        });
-      }
-      res.status(200).json(doc);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
+router.get("/:productId", ProductsController.products_get_product);
 
-router.patch("/:productId", checkAuth, (req, res, next) => {
-  const id = req.params.productId;
-  const updateOps = {};
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
-  Product.update({ _id: id }, { $set: updateOps })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Product Updated",
-        request: {
-          type: "GET",
-          url: "http://localhost:3000/products/" + id,
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
+router.patch("/:productId", checkAuth, ProductsController.products_update_product);
 
 router.delete("/:productId", checkAuth, (req, res, next) => {
   const id = req.params.productId;
