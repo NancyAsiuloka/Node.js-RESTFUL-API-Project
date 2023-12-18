4deab86f3f71af91940ecde2f3d443b1d46dc5b1
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-18 04:52:54 +0100

    added some functions

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product')
-
+const mongoose = require('mongoose')
 
 exports.products_get_all = (req, res, next) => {
     Product.find()
@@ -67,5 +67,57 @@ exports.products_get_all = (req, res, next) => {
         res.status(500).json({
           error: err,
         });
+      });
+  }
+
+  exports.products_get_product = (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+      .select("name price _id productImage")
+      .exec()
+      .then((doc) => {
+        console.log(doc);
+        if (doc) {
+          res.status(200).json({
+            product: doc,
+            request: {
+              type: "GET",
+              description: "Get all products",
+              url: "http://localhost:3000/products",
+            },
+          });
+        } else {
+          res.status(404).json({
+            message: "No valid entry found for provided ID",
+          });
+        }
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+  }
+
+  exports.products_update_product = (req, res, next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value;
+    }
+    Product.update({ _id: id }, { $set: updateOps })
+      .exec()
+      .then((result) => {
+        res.status(200).json({
+          message: "Product Updated",
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products/" + id,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: err });
       });
   }
